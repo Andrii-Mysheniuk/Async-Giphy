82e0de186bab8903e16b8b9ac56704295ab25090
Andrii-Mysheniuk <amysheniuk@webfx.co.uk>
2024-05-08 11:23:24 +0100

    add task solution

--- index.js
+++ index.js
@@ -1,4 +1,23 @@
-require('dotenv').config();
+import dotenv from 'dotenv';
+dotenv.config();
+import fetch from 'node-fetch';
 
-// Print out value of API key stored in .env file
-console.log(process.env.API_KEY)
+async function getImage(query) {
+  const endpoint = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}&q=${query}&limit=25&offset=0&rating=g&lang=en`;
+
+  try {
+    const response = await fetch(endpoint);
+    const data = await response.json();
+
+    console.log(data.data[0].images.original)
+
+    const gifUrl = data.data[0].images.original.url;
+    console.log("GIF URL:", gifUrl);
+    return gifUrl;
+  } catch (error) {
+    console.error("Error fetching GIF:", error);
+    return null;
+  }
+}
+
+getImage("cats");
